94b30cd8b1bc7fa660b91ea2b5c49523b66aa329
Anita Beauchamp <amadamspr@gmail.com>
2018-07-29 22:36:27 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -70,7 +70,8 @@ class Owner
   end
 
   def list_pets
-
+    
+    "I have #{self.pets[:fishes].size} fish, #{self.pets[:dogs].size} dog(s), and #{self.pets[:cats].size} cat(s)."
   end
 
   def self.all
